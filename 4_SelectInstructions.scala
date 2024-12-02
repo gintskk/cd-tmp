bed5446d14ce390431965996fbe0cd995a4fb6dd
Gints Kuļikovskis <gintskk@gmail.com>
2024-12-02 21:33:14 +0900

    Update 4 to remove temporary vars

--- 4_SelectInstructions.scala
+++ 4_SelectInstructions.scala
@@ -16,34 +16,33 @@ object Select {
     })
   }
 
-  def selectTail(t: Tail): List[Instruction] = t match {
-    case Seq(s, t) => selectStmt(s) ++ selectTail(t)
-    case Return(e) => selectStmt(Assign("return_temp", e)) ++ List(Instr(Movq(), List(XVar("return_temp"), Reg(RAX()))))
+  private def selectTail(t: Tail): List[Instruction] = t match {
+    case Seq(s, t) => selectStatement(XVar(s.asInstanceOf[Assign].x), s.asInstanceOf[Assign].e) ++ selectTail(t)
+    case Return(e) => selectStatement(Reg(RAX()), e)
   }
 
-  def selectStmt(s: Stmt): List[Instruction] = s match {
-    case Assign(x, e) => e match {
-      case Num(i) => List(Instr(Movq(), List(Imm(i), XVar(x))))
-      case Var(v) => List(Instr(Movq(), List(XVar(v), XVar(x))))
-      case Prim(op, es) => op match {
-        case Plus() => es(1) match {
-          case value: Var if value.x == x => // var = (+ a1 var)
-            List(Instr(Addq(), List(selectAtm(es(0)), XVar(x))))
-          case _ => //var = (+ a1 a2)
-            List(Instr(Movq(), List(selectAtm(es(0)), XVar(x))), Instr(Addq(), List(selectAtm(es(1)), XVar(x))))
-        }
-        case Minus() => if (es.length == 2) {
-          List(Instr(Movq(), List(selectAtm(es(0)), XVar(x))), Instr(Subq(), List(selectAtm(es(1)), XVar(x))))
-        } else {
-          List(Instr(Movq(), List(selectAtm(es(0)), XVar(x))), Instr(Negq(), List(XVar(x))))
-        }
-        case Read() => List(Callq("_read_int", 0), Instr(Movq(), List(Reg(RAX()), XVar(x))))
-        case Print() => List(Instr(Movq(), List(selectAtm(es(0)), XVar(x))), Instr(Movq(), List(selectAtm(es(0)), Reg(RDI()))), Callq("_print_int", 1))
+  private def selectStatement(x: Arg, e: Expr): List[Instruction] = e match {
+    case Num(i) => List(Instr(Movq(), List(Imm(i), x)))
+    case Var(v) => List(Instr(Movq(), List(XVar(v), x)))
+    case Prim(op, es) => op match {
+      case Plus() => es(1) match {
+        case value: Var if x.isInstanceOf[XVar] && value.x == x.asInstanceOf[XVar].x =>
+          // var = (+ a1 var)
+          List(Instr(Addq(), List(selectAtm(es(0)), x)))
+        case _ => //var = (+ a1 a2)
+          List(Instr(Movq(), List(selectAtm(es(0)), x)), Instr(Addq(), List(selectAtm(es(1)), x)))
       }
+      case Minus() => if (es.length == 2) {
+        List(Instr(Movq(), List(selectAtm(es(0)), x)), Instr(Subq(), List(selectAtm(es(1)), x)))
+      } else {
+        List(Instr(Movq(), List(selectAtm(es(0)), x)), Instr(Negq(), List(x)))
+      }
+      case Read() => List(Callq("_read_int", 0), Instr(Movq(), List(Reg(RAX()), x)))
+      case Print() => List(Instr(Movq(), List(selectAtm(es(0)), x)), Instr(Movq(), List(selectAtm(es(0)), Reg(RDI()))), Callq("_print_int", 1))
     }
   }
 
-  def selectAtm(e: Expr): Arg = e match {
+  private def selectAtm(e: Expr): Arg = e match {
     case Num(i) => Imm(i)
     case Var(x) => XVar(x)
     case _ => throw new RuntimeException()
